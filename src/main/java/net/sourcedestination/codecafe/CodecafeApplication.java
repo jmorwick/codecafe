d6b29ae3b68adf3cd7f002dc09f91db253dbcad0
jmorwick <jbmorwick@gmail.com>
2018-11-19 12:25:42 -0600

    started working towards oauth2

--- src/main/java/net/sourcedestination/codecafe/CodecafeApplication.java
+++ src/main/java/net/sourcedestination/codecafe/CodecafeApplication.java
@@ -2,10 +2,12 @@ package net.sourcedestination.codecafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ImportResource;
 
 @ImportResource("classpath:exercises.xml")
 @SpringBootApplication
+//@EnableOAuth2Sso
 public class CodecafeApplication {
 
     public static void main(String[] args) {
