dc33e582d07f31b0deaa85d9704e41a8c3e2b166
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-08-16 00:09:20 -0500

    updated front page to include exercise links #20

--- src/main/java/net/sourcedestination/codecafe/structure/ProfileController.java
+++ src/main/java/net/sourcedestination/codecafe/structure/ProfileController.java
@@ -1,5 +1,8 @@
 package net.sourcedestination.codecafe.structure;
 
+import net.sourcedestination.codecafe.structure.exercises.ExerciseDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class ProfileController {
+
+    @Autowired
+    private ApplicationContext appContext;
 
     @GetMapping("/")
     public String viewExercise(Map<String, Object> model,
@@ -19,6 +26,13 @@ public class ProfileController {
         if(principal != null) {
             model.put("username", principal.getName());
         }
+
+        Map<String, ExerciseDefinition> exercises = appContext.getBeansOfType(ExerciseDefinition.class);
+        model.put("exercises", // find id's of all exercises
+                exercises.values().stream()
+                    .map(ExerciseDefinition::getId)
+                    .collect(Collectors.toList())
+        );
 
         return "splash.html";
     }
