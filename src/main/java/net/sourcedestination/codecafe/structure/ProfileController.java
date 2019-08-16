085fa3fbb53ff4026eba0f02151b6a0726dda68a
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-08-16 00:15:32 -0500

    removed carets from un-expandable goals

--- src/main/java/net/sourcedestination/codecafe/structure/ProfileController.java
+++ src/main/java/net/sourcedestination/codecafe/structure/ProfileController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.Map;
