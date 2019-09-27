d4319e809376911031a451b7717525a5656404ca
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-09-26 22:37:05 -0500

    updated data driven test

--- src/test/java/net/sourcedestination/codecafe/structure/restrictions/RestrictionTests.java
+++ src/test/java/net/sourcedestination/codecafe/structure/restrictions/RestrictionTests.java
@@ -1,3 +1,4 @@
+
 package net.sourcedestination.codecafe.structure.restrictions;
 
 import jdk.jshell.Snippet;
@@ -9,44 +10,80 @@ import net.sourcedestination.codecafe.structure.exercises.ExerciseDefinition;
 import net.sourcedestination.codecafe.structure.goals.GoalStructure;
 import org.junit.Test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import static org.junit.Assert.*;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@RunWith(Parameterized.class)
 public class RestrictionTests {
 
-    private JShellExerciseTool tool =
-            new JShellExerciseTool("testuser", "1",
-            new InMemoryDBManager(),
-            1000,
-            new TestMessagingTemplate(),
-            new ExerciseDefinition(
-                    "1", "desc", 1000, "",
-                    Collections.emptyList(), new GoalStructure("", "",
-                    new TestGoal())
-            ));
+    public static List<String> EXPRESSIONS = List.of("5",  "2 + 5");
+    public static List<String> VARIABLE_DEFINITIONS = List.of(
+            "int add(int x, int y) { return x + y; }",
+            "void doNothing() {}"
+    );
+    private List<Snippet.Kind> whiteList;
+    private List<Snippet> validSnippets;
+    private List<Snippet> invalidSnippets;
 
-    @Test public void testClassExpressionSnippet() {
-        var restriction = new SnippetTypeWhiteList(Snippet.Kind.EXPRESSION);
-        var code = "class Foo { };";
-        var snippets = tool.getShell().sourceCodeAnalysis().sourceToSnippets(code);
-        assertTrue(restriction.test(snippets.get(0), tool));
+    public RestrictionTests(List<Snippet.Kind> whiteList,
+                            List<String> validSnippets,
+                            List<String> invalidSnippets) {
+        this.whiteList = whiteList;
+
+        tool =
+                new JShellExerciseTool("testuser", "1",
+                        new InMemoryDBManager(),
+                        1000,
+                        new TestMessagingTemplate(),
+                        new ExerciseDefinition(
+                                "1", "desc", 1000, "",
+                                Collections.emptyList(), new GoalStructure("", "",
+                                new TestGoal())
+                        ));
+
+        this.validSnippets = validSnippets.stream()
+                .map(s -> tool.getShell().sourceCodeAnalysis().sourceToSnippets(s).get(0))
+                .collect(Collectors.toList());
+        this.invalidSnippets = invalidSnippets.stream()
+                .map(s -> tool.getShell().sourceCodeAnalysis().sourceToSnippets(s).get(0))
+                .collect(Collectors.toList());
     }
 
-    @Test public void testErroneousExpressionSnippet() {
-        var restriction = new SnippetTypeWhiteList(Snippet.Kind.ERRONEOUS);
-        // Correctly Filtered It
-        var code = "SELECT * FROM testUser Where testUser = ' ' OR '1' = '1'";
-        var snippet = tool.getShell().sourceCodeAnalysis().sourceToSnippets(code);
-        assertFalse(restriction.test(snippet.get(0), tool));
-        assertFalse(true);
+    @Parameters public static Collection<Object[]> parameters() {
+        return List.of(
+                new Object[]{
+                        List.of(Snippet.Kind.EXPRESSION),
+                        VARIABLE_DEFINITIONS,
+                        EXPRESSIONS},
+                new Object[]{
+                        List.of(Snippet.Kind.METHOD),
+                        EXPRESSIONS,
+                        VARIABLE_DEFINITIONS}
+        );
     }
 
-    @Test public void testAcceptableExpressionSnippet() {
-        var restriction = new SnippetTypeWhiteList(Snippet.Kind.EXPRESSION);
-        var code = "5 + 1 == 3";
-        var snippet = tool.getShell().sourceCodeAnalysis().sourceToSnippets(code);
-        assertFalse(restriction.test(snippet.get(0), tool));
+    private JShellExerciseTool tool;
+
+    @Test public void testValidSnippets() {
+        var restriction = new SnippetTypeWhiteList(whiteList.toArray(new Snippet.Kind[0]));
+        validSnippets.forEach(s ->
+                assertTrue(s.kind() + " should be filtered for whitelist " + whiteList + ": " + s.source(),
+                        restriction.test(s, tool)));
     }
+
+    @Test public void testInvalidSnippets() {
+        var restriction = new SnippetTypeWhiteList(whiteList.toArray(new Snippet.Kind[0]));
+        invalidSnippets.forEach(s -> {
+                assertFalse(s.kind() + " should not be filtered for whitelist " + whiteList + ": " + s.source(),
+                        restriction.test(s, tool));
+        });
+    }
+
 }
-
